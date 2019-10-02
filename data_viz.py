0aa23f5ff4e6dfc75a5909d1c39d51692960ef93
Qing Yang <qyang.kevin@gmail.com>
2019-10-02 17:23:49 -0600

    working version of the code, no test, no documentation

--- data_viz.py
+++ data_viz.py
@@ -1,11 +1,9 @@
-import sys
 import matplotlib
 import matplotlib.pyplot as plt
-import math_lib as ml
 matplotlib.use('Agg')
 
 
-def boxplot(A, out_file_name):
+def boxplot(L, xticlabel, xlabel, geneName, out_file_name):
     '''
     Given a numerical array, make a box plot and save as png file
     ---
@@ -17,60 +15,17 @@ def boxplot(A, out_file_name):
     Output
     A png file with the plot
     '''
-    plt.boxplot(A)
-    plt.ylabel('Value Distribution')
-    plt.xlabel('Catagory')
-    plt.title('Mean: ' + str(ml.list_mean(A))
-              + ' '
-              + 'Stdev: ' + str(ml.list_stdev(A)))
+    width = 10
+    height = 3
+    fig = plt.figure(figsize=(width, height), dpi=300)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.boxplot(L)
+    ax.set_xticklabels(xticlabel, rotation=90)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel("Gene read counts")
+    ax.set_title('Gene: '+ geneName)
+
     plt.savefig(out_file_name, bbox_inches='tight')
-    pass
+    plt.close()
 
-
-def histogram(A, out_file_name):
-    '''
-    Given a numerical array, make a histogram and save as png file
-    ---
-    Input: A
-    An array containing numerical values
-    Input: out_file_name
-    Output file name
-    ---
-    Output
-    A png file with the plot
-    '''
-    plt.hist(A, bins=50)
-    plt.ylabel('Frequency')
-    plt.xlabel('Value')
-    plt.title('Mean: ' + str(ml.list_mean(A))
-              + ' '
-              + 'Stdev: ' + str(ml.list_stdev(A)))
-    plt.savefig(out_file_name, bbox_inches='tight')
-    pass
-
-
-def combo(A, out_file_name):
-    '''
-    Given a numerical array, make a box plot and
-    a histogram and save as png file
-    ---
-    Input: A
-    An array containing numerical values
-    Input: out_file_name
-    Output file name
-    ---
-    Output
-    A png file with the plots
-    '''
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.boxplot(A)
-    ax1.set_xlabel('Catagory')
-    ax1.set_ylabel('Value Distribution')
-    ax2.hist(A)
-    ax2.set_xlabel('Value')
-    ax2.set_ylabel('Frequency')
-    fig.suptitle('Mean: ' + str(ml.list_mean(A))
-                 + ' '
-                 + 'Stdev: ' + str(ml.list_stdev(A)))
-    fig.savefig(out_file_name, bbox_inches='tight')
-    pass
+    return 0
--- plot_gtex.py
+++ plot_gtex.py
@@ -32,45 +32,61 @@ def binary_search(key, D):
 
 
 def linear_search_all_hits (key, L):
-	'''
-	Gives indexs not values
-	'''
-	hit = []
-	for i in range(len(L)):
-		if key == L[i]:
-			hit.append(i)
-	return hit
+    '''
+    Gives indexs not values
+    '''
+    hit = []
+    for i in range(len(L)):
+        if key == L[i]:
+            hit.append(i)
+    return hit
 
 
 def main():
     meta_data_file_name = "GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt"
-    rna_data_file_name="GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz"
+    rna_data_file_name = "GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz"
+    target_type = "SMTS"
+    target_gene_name = "ACTA2"
+    out_file_name = target_gene_name + "_" + target_type + "_boxplot.png"
+
+
+    #meta_data_file_name = args.meta
+    #rna_data_file_name = args.counts
+    #tissue_type = args.tissue
+    #target_gene_name = args.gene
 
     SAMPID = []
     SMTS = []
-    SMTSD = []
-
     metadata_header = None
-
+    sample_info = None
+    tissue_group = []
+    xlabels = []
+    # This is a 2D array, first layer stores tissue groups, second stores
+    # all the sample IDs that belong to that group
+    categoraized_ids = []
+    # Processing the meta data file
     for l in open(meta_data_file_name):
-    	# Split lines by tab and stored them in array
-    	# rstrip() returns a copy of the string with trailing characters removed
-    	sample_info = l.rstrip().split("\t")
+        # Store the first row i.e. header in a separate array
+        if metadata_header is None:
+            metadata_header = l.rstrip().split("\t")
+            SAMPID_idx = linear_search("SAMPID", metadata_header)
+            SMTS_idx = linear_search(target_type, metadata_header)
+        else:
+            # Split lines by tab and stored them in array
+            # rstrip() returns a copy of the string with trailing characters removed
+            sample_info = l.rstrip().split("\t")
+            sample_id = sample_info[SAMPID_idx]
+            tissue_type = sample_info[SMTS_idx]
+            # Try to find the tissue type in the existing list
+            tissue_idxs = linear_search(tissue_type, tissue_group)
+            # If not found, add that tissue type to the list
+            if tissue_idxs == -1:
+                xlabels.append(tissue_type)
+                tissue_idxs = len(tissue_group)
+                tissue_group.append(tissue_type)
+                categoraized_ids.append([])
+            categoraized_ids[tissue_idxs].append(sample_id)
 
-    	# Store the first row i.e. header in a separate array
-    	if metadata_header is None:
-    		metadata_header = sample_info
-    		continue
-
-    	SAMPID_idx = linear_search("SAMPID", metadata_header)
-    	SMTS_idx = linear_search("SMTS", metadata_header)
-    	SMTSD_idx = linear_search("SMTSD", metadata_header)
-
-    	#print (sample_info[SAMPID_idx],sample_info[SMTS_idx],sample_info[SMTSD_idx])
-
-    	SAMPID.append(sample_info[SAMPID_idx])
-    	SMTS.append(sample_info[SMTS_idx])
-    	SMTSD.append(sample_info[SMTSD_idx])
 
     # For unit test, no need to be random
     # Test if the return values match, and the length
@@ -80,52 +96,50 @@ def main():
     version = None
     dim = None
     rna_header = None
-    target_gene_name = "SDHC"
+    counts = [[] for i in range(len(tissue_group))]
 
+    # Processing the count file
     # use gzip.open to read gzip file
     for l in gzip.open(rna_data_file_name,"rt"):
-    	if version == None:
-    		version = l
-    		continue
+        # Assume first line stores the version
+        if version == None:
+            version = l
+            continue
+        # Assume second line stores the dimension
+        if dim == None:
+            dim = l
+            continue
+        # Assume thrid line stores the header for counts
+        if rna_header == None:
+            rna_header = l.rstrip().split("\t")
+            # Use tuple to store the original index
+            # and then sort based on the first element in tuple
+            rna_header_plus_index = []
+            for i in range(len(rna_header)):
+                rna_header_plus_index.append([rna_header[i],i])
+            rna_header_plus_index.sort(key=lambda pair: pair[0])
+            # Store the index of description
+            Description_idx = binary_search("Description",
+                                            rna_header_plus_index)
+            if Description_idx == -1:
+                sys.write('Description not found in header.')
 
-    	if dim == None:
-    		dim = l
-    		continue
+        else :
+            rna_counts = l.rstrip().split("\t")
 
-    	if rna_header == None:
-    		rna_header = l.rstrip().split("\t")
-    		continue
+            if rna_counts[Description_idx] == target_gene_name:
+                # For each tissue type
+                for tissue_idx in range(len(tissue_group)):
+                    # For each individual in the same tissue type
+                    for sample in categoraized_ids[tissue_idx]:
+                        rna_header_idx = binary_search(sample, rna_header_plus_index)
+                        # Some of the sampeles do not have rna counts
+                        if rna_header_idx != -1:
+                            count = int(rna_counts[rna_header_idx])
+                            counts[tissue_idx].append(count)
+                break
 
-    	# Use tuple to store the original index and then sort based on the first element in tuple, ie the header
-    	rna_header_plus_index = []
-    	for i in range(len(rna_header)):
-    		rna_header_plus_index.append([rna_header[i],i])
-    	rna_header_plus_index.sort(key=lambda pair: pair[0])
-
-    	rna_counts = l.rstrip().split("\t")
-
-    	Description_idx = linear_search("Description", rna_header)
-
-    	if Description_idx == -1:
-    		sys.write('Description not found in header.')
-
-    	if rna_counts[Description_idx] == target_gene_name:
-    		# Obtain the index in the metadata to get all the names
-    		blood_idxs = linear_search_all_hits('Blood',SMTS)
-
-    		# Extract the read counts from rna_counts
-    		blood_counts = []
-    		for blood_idxs in blood_idxs:
-    			#rna_header_idx = linear_search(SAMPID[blood_idxs],rna_header)
-    			rna_header_idx = binary_search(SAMPID[blood_idxs], rna_header_plus_index)
-    			# Some of the sampeles do not have rna counts
-    			if rna_header_idx != -1:
-    				count = int(rna_counts[rna_header_idx])
-    				blood_counts.append(count)
-    		break
-
-
-    dv.boxplot(blood_counts, 'test.png')
+    dv.boxplot(counts, xlabels, target_type, target_gene_name, out_file_name)
 
 if __name__ == '__main__':
     main()
